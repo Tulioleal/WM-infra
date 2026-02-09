78172ca45e4c13d61345846e6baca8d71ce525a9
Tulioleal <tuliolealdev@gmail.com>
2026-02-09 18:02:39 -0300

    Add GCS_DATASETS_BUCKET to infra_config ConfigMap in Kubernetes resources

--- k8s.tf
+++ k8s.tf
@@ -43,3 +43,16 @@ resource "kubernetes_service_account_v1" "app_sa" {
     }
   }
 }
+
+resource "kubernetes_config_map_v1" "infra_config" {
+  metadata {
+    name      = "infra-config"
+    namespace = kubernetes_namespace_v1.waste_detection.metadata[0].name
+  }
+
+  data = {
+    GCS_MODELS_BUCKET   = google_storage_bucket.models.name
+    GCS_IMAGES_BUCKET   = google_storage_bucket.inference_images.name
+    GCS_DATASETS_BUCKET = google_storage_bucket.datasets.name
+  }
+}
