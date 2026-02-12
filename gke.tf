158e7314e34b3c7321dc771b66f84fbb152d771e
Tulioleal <tuliolealdev@gmail.com>
2026-02-12 17:41:58 -0300

    remove unused training pool

--- gke.tf
+++ gke.tf
@@ -92,59 +92,6 @@ resource "google_container_node_pool" "inference_pool" {
   }
 }
 
-# ============================================================================
-# Node Pool para Entrenamiento (GPU - Opcional)
-# ============================================================================
-
-resource "google_container_node_pool" "training_pool" {
-  name       = "training-pool"
-  location   = var.zone
-  cluster    = google_container_cluster.primary.name
-  initial_node_count = 0  # Escala desde 0
-  
-  autoscaling {
-    min_node_count = 0
-    max_node_count = 2
-  }
-  
-  node_config {
-    preemptible  = false // esto deberia ser true para reducir costos, pero las GPU preemptibles pueden ser inestables para entrenamiento, para efectos practicos lo dejamos en false
-    service_account = google_service_account.gke_nodes.email
-    // cambiar a n1-standard-4 y tesla t4 para entrenamiento pero hay poca disponibilidad, por eso usamos e2-standard-4s
-    machine_type = "g2-standard-4"  # 4 vCPU, 15 GB RAM
-    disk_size_gb = 100
-    disk_type    = "pd-ssd"
-    
-    # GPU Tesla T4 para entrenamiento
-    guest_accelerator {
-      type  = "nvidia-l4"
-      count = 1
-      gpu_driver_installation_config {
-        gpu_driver_version = "LATEST"
-      }
-    }
-    
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
-    
-    labels = {
-      workload = "training"
-      env      = var.environment
-    }
-    
-    taint {
-      key    = "workload"
-      value  = "training"
-      effect = "NO_SCHEDULE"
-    }
-    
-    workload_metadata_config {
-      mode = "GKE_METADATA"
-    }
-  }
-}
-
 resource "google_container_node_pool" "system_pool" {
   name       = "system-pool"
   location   = var.zone
